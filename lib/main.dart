c85edbf259f93355f6714f52cc60753a920db89c
aj-xy <ajayshanthanu@gmail.com>
2024-03-26 13:43:42 +0530

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:whatsapp/firebase_options.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Myapp());
+    return MaterialApp(home: Myapp());
   }
 }
+
 
 class Myapp extends StatefulWidget {
   const Myapp({super.key});
